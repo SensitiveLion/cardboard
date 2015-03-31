73a8c8d4aec48fce74e05dc0d84cd1bb83d60cb1
Paul Goodman <paulsaywardgoodman@gmail.com>
2015-03-31 15:20:41 -0400

    Update factory_girl.rb

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -2,8 +2,8 @@ require 'factory_girl'
 
 FactoryGirl.define do
   factory :user do
-    sequence(:username) {|n| "user#{n}"}
-    sequence(:email) {|n| "user#{n}@example.com" }
+    sequence(:username) { |n| "user#{n}"}
+    sequence(:email) { |n| "user#{n}@example.com" }
     password 'password'
     password_confirmation 'password'
   end
