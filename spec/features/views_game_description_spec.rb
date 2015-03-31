e006a3fc945073bae2950d66ddc9cdea25a8def5
Matthew Riggott <MatthewSRiggott@gmail.com>
2015-03-31 16:48:02 -0400

    Fixed Travis indentation

--- spec/features/views_game_description_spec.rb
+++ spec/features/views_game_description_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 feature 'site visitor views the details of a game' do
   let(:game) { FactoryGirl.create(:game) }
-    scenario 'visitor views game details' do
-      game
-      visit games_path
-      click_link game.name
-      expect(page).to have_content(game.name)
-      expect(page).to have_content(game.description)
-      expect(page).to have_content(game.min_players)
-      expect(page).to have_content(game.max_players)
-      expect(page).to have_content(game.playing_time)
-      expect(page).to have_content(game.complexity)
-    end
+  scenario 'visitor views game details' do
+    game
+    visit games_path
+    click_link game.name
+    expect(page).to have_content(game.name)
+    expect(page).to have_content(game.description)
+    expect(page).to have_content(game.min_players)
+    expect(page).to have_content(game.max_players)
+    expect(page).to have_content(game.playing_time)
+    expect(page).to have_content(game.complexity)
   end
+end
