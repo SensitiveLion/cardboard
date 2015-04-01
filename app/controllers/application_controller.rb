d1774cef18cf65d7df9c00afe57c856f833440c0
SensitiveLion <andypassidomo@gmail.com>
2015-04-01 14:58:09 -0400

    fixed sintax issues in aplication contoller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,15 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |user| user.permit(:username,
-      :email, :password, :password_confirmation, :location, :age, :first_name,
-      :last_name, :profile_photo) }
-    devise_parameter_sanitizer.for(:account_update) { |user| user.permit( :username,
-      :password, :password_confirmation, :location, :age, :first_name, :last_name,
-      :profile_photo, :current_password) }
+    devise_parameter_sanitizer.for(:sign_up) do |user|
+      user.permit(:username, :email, :password, :password_confirmation,
+      :location, :age, :first_name, :last_name, :profile_photo)
+    end
+    devise_parameter_sanitizer.for(:account_update) do |user|
+      user.permit(:username, :email, :password, :password_confirmation,
+      :location, :age, :first_name, :last_name, :profile_photo,
+      :current_password)
+    end
   end
 
   protect_from_forgery with: :exception
