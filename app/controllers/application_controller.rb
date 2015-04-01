e58b31c83b2f653d2b31be6571315cb9e4e9ed94
SensitiveLion <andypassidomo@gmail.com>
2015-04-01 14:48:59 -0400

    Added ability to edit profile and profile picture and add pictre uploading to games.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-   protected
+  protected
 
-   def configure_permitted_parameters
-     devise_parameter_sanitizer.for(:sign_up) { |user| user.permit(:username,
-       :email, :password, :password_confirmation, :location, :age, :first_name,
-       :last_name, :profile_photo) }
-     devise_parameter_sanitizer.for(:account_update) << :profile_photo
-   end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |user| user.permit(:username,
+      :email, :password, :password_confirmation, :location, :age, :first_name,
+      :last_name, :profile_photo) }
+    devise_parameter_sanitizer.for(:account_update) { |user| user.permit( :username,
+      :password, :password_confirmation, :location, :age, :first_name, :last_name,
+      :profile_photo, :current_password) }
+  end
 
   protect_from_forgery with: :exception
 end
