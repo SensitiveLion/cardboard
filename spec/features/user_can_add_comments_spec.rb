5c8aced93754e140365835841efdb1b599593b18
SensitiveLion <andypassidomo@gmail.com>
2015-04-02 16:01:38 -0400

    Fix comment cancel test

--- spec/features/user_can_add_comments_spec.rb
+++ spec/features/user_can_add_comments_spec.rb
@@ -4,16 +4,17 @@ require 'helpers'
 feature 'user can add comments to reviews' do
   let(:game) { FactoryGirl.create(:game) }
   let(:user) { FactoryGirl.create(:user) }
+  let(:review) { FactoryGirl.create(:review) }
 
   scenario 'user can add comments to a review' do
     sign_in_as(user)
-    visit game_path(game)
-    add_review
+    visit game_path(review.game)
     click_link "add comment"
     fill_in "your comment", with: "something something something"
     click_button "add comment"
     expect(page).to have_content("comment submitted.")
   end
+
   scenario 'visitor cannot add comments' do
     visit new_game_review_path(game)
     expect(page).to have_content(
@@ -21,14 +22,13 @@ feature 'user can add comments to reviews' do
     )
   end
 
-  scenario 'user can add comments to a review' do
+  scenario 'user can cancel a comment' do
     sign_in_as(user)
-    visit game_path(game)
-    add_review
+    visit game_path(review.game)
     click_link "add comment"
     fill_in "your comment", with: "something to be cancelled"
     click_button "cancel"
-    expect(page).to have_content(game.name)
+    expect(page).to have_content(review.game.name)
     expect(page).not_to have_content("something to be cancelled")
   end
 
