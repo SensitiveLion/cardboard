666724e070d327dd5b51ca7b1bee341f90f94a56
Kayla <kayla.comalli@gmail.com>
2015-04-06 15:32:26 -0400

    requiring ActionController

--- spec/features/user_gets_notification_emails_spec.rb
+++ spec/features/user_gets_notification_emails_spec.rb
@@ -1,13 +1,18 @@
-scenario "review a game" do
-  ActionMailer::Base.deliveries.clear
+require 'rails_helper'
+require 'helpers'
 
-  game = FactoryGirl.create(:game)
-  user = FactoryGirl.create(:user)
+feature "Gets emails for new game reviews" do
+  scenario "game user gets review emails" do
+    ActionMailer::Base.deliveries.clear
 
-  sign_in_as(user)
-  visit new_game_review_path(game)
-  fill_in "your review", with: "Total garbage."
-  click_button "add review"
-  expect(page).to have_content("Total garbage.")
-  expect(ActionMailer::Base.deliveries.count).to eq(1)
+    game = FactoryGirl.create(:game)
+    user = FactoryGirl.create(:user)
+
+    sign_in_as(user)
+    visit new_game_review_path(game)
+    fill_in "your review", with: "Total garbage."
+    click_button "add review"
+    expect(page).to have_content("Total garbage.")
+    expect(ActionMailer::Base.deliveries.count).to eq(1)
+  end
 end
