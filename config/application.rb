2d52ccc4df8c2b49a2611a019f6d52a88f2cbc3a
Kayla <kayla.comalli@gmail.com>
2015-04-07 13:41:46 -0400

    making it pretty

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,10 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module Cardboard
   class Application < Rails::Application
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
