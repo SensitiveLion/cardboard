2ef597585e7fea166f9b33110e747b18f0676156
Paul Goodman <paulsaywardgoodman@gmail.com>
2015-04-09 13:20:21 -0400

    Refactor mods.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,15 +8,21 @@ class UsersController < ApplicationController
   end
 
   def update
+    before_action :authenticate_admin!
     @user = User.find(params[:id])
-    if current_user.authority == "admin"
-      if @user.authority == "user"
-        @user.authority = "mod"
-      elsif @user.authority == "mod"
-        @user.authority = "user"
-      end
-      @user.save
+    if @user.authority == "user"
+      @user.authority = "mod"
+    elsif @user.authority == "mod"
+      @user.authority = "user"
     end
+    @user.save
     redirect_to user_path(@user)
+  end
+
+  protected
+
+  def authenticate_admin!
+  if !user_signed_in? || current_user.authority != "admin"
+    raise ActionController::RoutingError.new("Not Found")
   end
 end
