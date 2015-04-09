60e95ed4c8d4cf32ed9a92bbcf8a499e155db9de
Kayla <kayla.comalli@gmail.com>
2015-04-09 15:56:37 -0400

    added tests for user status

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,11 +4,39 @@ require 'helpers'
 describe User do
   let(:user) { FactoryGirl.create(:user) }
 
-  describe 'new user profile has attributes' do
-    context 'user scoring' do
-      it 'user has default XP and level status' do
+  describe 'can have first name, last name, location and age' do
+    let(:user) { FactoryGirl.create(:user) }
+    context 'user has optional abilities' do
+      it 'has optional fields' do
+        expect(user.first_name).to eq "person"
+        expect(user.last_name).to eq "personson"
+        expect(user.location).to eq "here"
+        expect(user.age).to eq "20"
+      end
+    end
+  end
+  describe 'has required fields for user' do
+    let(:user) { FactoryGirl.create(:user) }
+    context 'user has email and username' do
+      it 'has required fields' do
+        expect(user.email).to eq "user#{1}@example.com"
+        expect(user.username).to eq "user#{1}"
+      end
+    end
+  end
+  describe 'karma score shows xp' do
+    let(:user) { FactoryGirl.create(:user) }
+    context 'user karma score shows xp' do
+      it 'shows xp' do
         expect(user.karma_score).to eq 0
-        expect(user.level).to eq 0
+      end
+    end
+  end
+  describe 'user level shows level' do
+    let(:user) { FactoryGirl.create(:user) }
+    context 'user has level' do
+      it 'shows level' do
+        expect(user.status).to eq "candyland newb"
       end
     end
   end
