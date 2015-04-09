f1872d0ed4ef0f380567f815a34c7d271b8bb685
Matthew Riggott <Matthew.S.Riggott@gmail.com>
2015-04-09 10:52:07 -0400

    removed attempt at oauth test- broke travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,39 +1,6 @@
 require 'coveralls'
 Coveralls.wear!('rails')
 
-RACK_ENV = ENV['ENVIRONMENT'] ||= 'test'
-
-def valid_facebook_login
-  if RACK_ENV == 'test'
-    OmniAuth.config.test_mode = true
-    fb_omniauth_hash =
-        {"provider"=>"facebook",
-     "uid"=>"123456789",
-     "info"=>
-      {"email"=>"bob@gmail.com",
-       "name"=>"Bob Smith",
-       "first_name"=>"Bob",
-       "last_name"=>"Smith",
-       "verified"=>true},
-     "credentials"=>
-      {"token"=>
-        "testingtoken91584hkdhf3",
-        "expires_at"=>(Time.now + 1.week)},
-     "extra"=>
-      {"raw_info"=>
-        {"id"=>"04296802934096",
-         "email"=>"bob@gmail.com",
-         "first_name"=>"Bob",
-         "gender"=>"male",
-         "last_name"=>"Smith",
-         "locale"=>"en_US",
-         "name"=>"Bob Smith",
-         "timezone"=>-4,
-         "updated_time"=>"2015-03-26T05:36:32+0000",
-         "verified"=>true}}}
-  end
-end
-
 OmniAuth.config.add_mock(:facebook, fb_omniauth_hash)
 
 RSpec.configure do |config|
